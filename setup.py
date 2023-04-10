ec21a90ad76f78e24ff409a427647b8a913df817
watchernyu <cw1681@nyu.edu>
2023-04-09 21:21:53 -0700

    remove dependencies that might cause conflict

--- setup.py
+++ setup.py
@@ -60,14 +60,9 @@ setup(
         ]
     },
     install_requires=[
-        "gym<0.24.0",
         "numpy",
         "mujoco_py",
         "pybullet",
         "h5py",
-        "termcolor",  # adept_envs dependency
-        "click",  # adept_envs dependency
-        "dm_control>=1.0.3",
-        "mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl",
     ],
 )
